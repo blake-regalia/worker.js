e3b4616d5f101871a35228989eb42d0eff37bf42
Andrey <andrey-ne@mail.ru>
2018-10-24 14:42:04 +0300

    fix: getRootModule
    worker fails if bundled with webpack (module.parent == null).

--- src/main/node/self.js
+++ src/main/node/self.js
@@ -5,8 +5,13 @@ const ipc = require('./ipc.js');
 // open socket
 let d_socket = new net.Socket({fd:4});
 
+// returns root module
+function getRootModule(module){
+	return module.parent ? getRootModule(module.parent) : module;
+}
+
 // create ipc
 module.exports = new ipc(d_socket, {
-	origin: module.parent.parent.parent.filename,
+	origin: getRootModule(module).filename,
 	args: process.argv.slice(2),
 });
